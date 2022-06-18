8c276f66d43eaf7bac50a601ddcdd6b30aa727fe
Shivang Sharma <83517625+shivang214@users.noreply.github.com>
2022-06-19 00:17:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,77 @@
-let loc = document.getElementById("location");
-let tempicon = document.getElementById("temp-icon");
-let tempvalue = document.getElementById("temp-value");
-let climate = document.getElementById("climate");
+let loc =document.getElementById("location");
+let tempicon=document.getElementById("temp-icon");
+let tempvalue=document.getElementById("temp-value");
+let climate =document.getElementById("climate");
 let iconfile;
-const searchInput = document.getElementById("search-input");
-const searchButton = document.getElementById("search-button");
+const searchInput=document.getElementById("search-input");
+const searchButton=document.getElementById("search-button");
+
+searchButton.addEventListener('click', (e)=>
+{
+
+e.preventDefault();
+getWeather(searchInput.value);
+searchInput.value='';
 
 
-
-searchButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    getWeathether(searchInput.value);
-    searchInput.value = '';
 });
 
 
-const getWeathether = async (city) => {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=21503e848a940e9ae4a20af90c186a99`,
-            { mode: 'cors' }
 
+const getWeather=async (city)=>
+{
+    try{
 
+        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dab3af44de7d24ae7ff86549334e45bd`,
+   
+            {mode: 'cors'}
         );
 
-
-        const weatherData = await response.json();
+        const weatherData= await response.json();
         console.log(weatherData);
-        const { name } = weatherData;
-        const { feels_like } = weatherData.main;
-        const { id, main } = weatherData.weather[0];
-        loc.textContent = name;
-        climate.textContent = main;
-        tempvalue.textContent = Math.round(feels_like - 273);
-        if (id < 300 && id > 200) {
-            tempicon.src = "thunder.svg";
+        const{name}=weatherData;
+        const{feels_like}=weatherData.main;
+        const{id,main}=weatherData.weather[0];
+        loc.textContent=name;
+        climate.textContent=main;
+        tempvalue.textContent=Math.round(feels_like-273);
+        if(id<300 && id>200)
+        {
+            tempicon.src="./icons/thunderstorm.svg"
         }
-        else if (id < 400 && id > 300) {
-            tempicon.src = "cloudy.svg";
+       else  if(id<400 && id>300)
+        {
+            tempicon.src="./icons/cloud-solid.svg"
         }
-        else if (id < 600 && id > 500) {
-            tempicon.src = "rainy-1.svg";
+       else if(id<600&& id>500)
+        {
+            tempicon.src="./icons/rain.svg"
         }
-        else if (id < 700 && id > 600) {
-            tempicon.src = "snowy-1.svg";
+       else  if(id<700 && id>600)
+        {
+            tempicon.src="./icons/snow.svg"
         }
-        else if (id < 800 && id > 700) {
-            tempicon.src = "weathe_sunset.svg";
+       else  if(id<800 && id>700)
+        {
+            tempicon.src="./icons/clouds.svg"
         }
+         else if(id==800)
+        {
+            tempicon.src="./icons/clouds-and-sun.svg"
+        }
+
+
+
+   
     }
-    catch (error) {
-        alert('city not found');
-    }
+catch(error)
+{
+    alert('city not found');
+}
+
+
+
+
 
 };
 
@@ -58,46 +79,93 @@ const getWeathether = async (city) => {
 
 
 
-window.addEventListener("load", () => {
-    let long;
-    let lat;
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            const proxy = "https://cors-anywhere.herokuapp.com/corsdemo";
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=21503e848a940e9ae4a20af90c186a99;
-            `
-            fetch(api).then((response) => {
-                return response.json();
-            })
-                .then(data => {
-                    const { name } = data;
-                    const { feels_like } = data.main;
-                    const { id, main } = data.weather[0];
 
 
-                    loc.textContent = name;
-                    climate.textContent = main;
-                    tempvalue.textContent = Math.round(feels_like - 273);
-                    if (id < 300 && id > 200) {
-                        tempicon.src = "thunder.svg";
-                    }
-                    else if (id < 400 && id > 300) {
-                        tempicon.src = "cloudy.svg";
-                    }
-                    else if (id < 600 && id > 500) {
-                        tempicon.src = "rainy-1.svg";
-                    }
-                    else if (id < 700 && id > 600) {
-                        tempicon.src = "snowy-1.svg";
-                    }
-                    else if (id < 800 && id > 700) {
-                        tempicon.src = "weathe_sunset.svg";
-                    }
-                })
+
+
+
+
+
+
+
+window.addEventListener("load" ,()=>{
+
+let long;
+let lat;
+
+if(navigator.geolocation)
+{
+
+    navigator.geolocation.getCurrentPosition((position)=>
+    {
+
+   
+    
+    long=position.coords.longitude;
+    lat=position.coords.latitude;
+    const proxy="https://cors-anywhere.herokuapp.com/";
+
+        const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=dab3af44de7d24ae7ff86549334e45bd     `
+
+        fetch(api).then((response)=>{
+
+            return response.json();
+
 
         })
-    }
+
+        .then (data =>
+            {
+
+                    const{name}=data;
+                    const{feels_like}=data.main;
+                    const{id,main}=data.weather[0];
+
+
+                    loc.textContent=name;
+                    climate.textContent=main;
+                    tempvalue.textContent=Math.round(feels_like-273);
+                    if(id<300 && id>200)
+                    {
+                        tempicon.src="./icons/thunderstorm.svg"
+                    }
+                   else  if(id<400 && id>300)
+                    {
+                        tempicon.src="./icons/cloud-solid.svg"
+                    }
+                   else if(id<600&& id>500)
+                    {
+                        tempicon.src="./icons/rain.svg"
+                    }
+                   else  if(id<700 && id>600)
+                    {
+                        tempicon.src="./icons/snow.svg"
+                    }
+                   else  if(id<800 && id>700)
+                    {
+                        tempicon.src="./icons/clouds.svg"
+                    }
+                     else if(id==800)
+                    {
+                        tempicon.src="./icons/clouds-and-sun.svg"
+                    }
+
+
+
+
+
+                    console.log(data);
+
+
+            })
+
+
+
+}
+    
+    
+    
+    )}
+
+
 })
